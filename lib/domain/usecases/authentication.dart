91467d422dcbe04900e9ac2d93d47e2c386cb233
paulocalazans <paulo.pc355@gmail.com>
2021-06-12 09:30:27 -0300

    move production code to thair on target

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -12,4 +12,6 @@ class AuthenticationParams {
     required this.email,
     required this.secret
   });
+
+  Map toJson() => { 'email': this.email, 'password': this.secret };
 }
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -4,28 +4,8 @@ import 'package:test/test.dart';
 
 import 'package:fordev/domain/usecases/usecases.dart';
 
-class RemoteAuthentication {
-  final HttpClient httpClient;
-  final String url;
-
-  RemoteAuthentication({required this.httpClient, required this.url});
-
-  Future<void>? auth(AuthenticationParams params) async {
-    final body = {
-      'email': params.email,
-      'password': params.secret
-    };
-    await httpClient.request(url: url, method: 'post', body: body );
-  }
-}
-
-abstract class HttpClient {
-  Future<void>? request({
-    required String url,
-    required String method,
-    Map<String, dynamic> body
-  });
-}
+import 'package:fordev/data/usecases/usecases.dart';
+import 'package:fordev/data/http/http.dart';
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
