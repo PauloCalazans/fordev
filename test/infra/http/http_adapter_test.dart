78bbc82798b323e523b83057be3e0188f99e1bd0
paulocalazans <paulo.pc355@gmail.com>
2021-06-15 20:47:07 -0300

    ensure HttpAdapter calls post without body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -22,10 +22,12 @@ class HttpAdapater {
       'content-type': 'application/json',
       'accept': 'application/json'
     };
+    final requestBody = body != null ? jsonEncode(body) : null;
+
     await client.post(
         Uri.parse(url),
         headers: headers,
-        body: jsonEncode(body)
+        body: requestBody
     );
   }
 }
@@ -56,6 +58,17 @@ void main() {
               'accept': 'application/json'
             },
             body: '{"any_key":"any_value"}'
+          )
+      );
+    });
+
+    test('Should call post without body', () async {
+      await sut.request(url: url, method: 'post');
+
+      verify(
+          client.post(
+              any,
+              headers: anyNamed('headers')
           )
       );
     });
