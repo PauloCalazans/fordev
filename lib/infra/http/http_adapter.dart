f01d34a3874ed1f6a50bfb3a8f97264c77035ac8
paulocalazans <paulo.pc355@gmail.com>
2021-06-16 20:19:35 -0300

    ensure HttpAdapater throws ServerError if post throws

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -20,14 +20,17 @@ class HttpAdapater implements HttpClient {
     };
     final requestBody = body != null ? jsonEncode(body) : null;
     var response = Response('', 500);
-    if(method == 'post') {
-      response = await client.post(
-          Uri.parse(url),
-          headers: headers,
-          body: requestBody
-      );
+    try {
+      if (method == 'post') {
+        response = await client.post(
+            Uri.parse(url),
+            headers: headers,
+            body: requestBody
+        );
+      }
+    } catch(error) {
+      throw HttpError.serverError;
     }
-
     return _handleResponse(response);
   }
 
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -38,6 +38,10 @@ void main() {
       mockRequest().thenAnswer((_) async => Response(body, statusCode));
     }
 
+    void mockError() {
+      mockRequest().thenThrow(Exception());
+    }
+
     setUp(() {
       mockResponse(200);
     });
@@ -140,6 +144,14 @@ void main() {
 
     test('Should return ServerError if post returns 500', () {
       mockResponse(500);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+
+    test('Should return ServerError if post throws', () {
+      mockError();
 
       final future = sut.request(url: url, method: 'post');
 
