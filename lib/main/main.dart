af4594c7dcc88d353f3b04b7a8e93631d414febd
paulocalazans <paulo.pc355@gmail.com>
2021-06-16 21:23:50 -0300

    add LoginPage layout

---
+++ lib/main/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+import '../ui/components/component.dart';
+
+void main() {
+  runApp(App());
+}
---
+++ lib/ui/pages/login_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../components/component.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            LoginHeader(),
+
+            Headline1(text: 'Login'),
+
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Form(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          icon: Icon(Icons.email, color: Theme.of(context).primaryColorLight)
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0, bottom: 32),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Senha',
+                              icon: Icon(Icons.lock, color: Theme.of(context).primaryColorLight)
+                          ),
+                          obscureText: true,
+                        ),
+                      ),
+
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Entrar'.toUpperCase())
+                      ),
+
+                      TextButton.icon(
+                          onPressed: () {},
+                          icon: Icon(Icons.person),
+                          label: Text('Criar Conta')
+                      )
+                    ],
+                  )
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
