fff93353dc2ae23b8fc1df6cd170913ed72c05ee
paulocalazans <paulo.pc355@gmail.com>
2021-06-18 22:11:48 -0300

    move EmailInput to its on file

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../components/components.dart';
+import 'components/components.dart';
 import 'login_presenter.dart';
 
 class LoginPage extends StatefulWidget {
@@ -49,59 +51,49 @@ class _LoginPageState extends State<LoginPage> {
 
                   Padding(
                     padding: const EdgeInsets.all(32),
-                    child: Form(
-                        child: Column(
-                          children: [
-                            StreamBuilder<String?>(
-                                stream: widget.presenter?.emailErrorStream,
-                                builder: (context, snapshot) {
-                                  return TextFormField(
-                                    decoration: InputDecoration(
-                                        labelText: 'Email',
-                                        icon: Icon(Icons.email, color: Theme.of(context).primaryColorLight),
-                                        errorText: snapshot.data?.isEmpty == true ? null : snapshot.data
-                                    ),
-                                    keyboardType: TextInputType.emailAddress,
-                                    onChanged: widget.presenter?.validateEmail,
-                                  );
-                                }
-                            ),
+                    child: Provider<LoginPresenter>(
+                      create: (_) => widget.presenter!,
+                      child: Form(
+                          child: Column(
+                            children: [
+                              EmailInput(),
 
-                            Padding(
-                              padding: const EdgeInsets.only(top: 8.0, bottom: 32),
-                              child: StreamBuilder<String?>(
-                                  stream: widget.presenter?.passwordErrorStream,
+                              Padding(
+                                padding: const EdgeInsets.only(top: 8.0, bottom: 32),
+                                child: StreamBuilder<String?>(
+                                    stream: widget.presenter?.passwordErrorStream,
+                                    builder: (context, snapshot) {
+                                      return TextFormField(
+                                        decoration: InputDecoration(
+                                            labelText: 'Senha',
+                                            icon: Icon(Icons.lock, color: Theme.of(context).primaryColorLight),
+                                            errorText: snapshot.data?.isEmpty == true ? null : snapshot.data
+                                        ),
+                                        obscureText: true,
+                                        onChanged: widget.presenter?.validatePassword,
+                                      );
+                                    }
+                                ),
+                              ),
+
+                              StreamBuilder<bool>(
+                                  stream: widget.presenter?.isFormValidErrorStream,
                                   builder: (context, snapshot) {
-                                    return TextFormField(
-                                      decoration: InputDecoration(
-                                          labelText: 'Senha',
-                                          icon: Icon(Icons.lock, color: Theme.of(context).primaryColorLight),
-                                          errorText: snapshot.data?.isEmpty == true ? null : snapshot.data
-                                      ),
-                                      obscureText: true,
-                                      onChanged: widget.presenter?.validatePassword,
+                                    return ElevatedButton(
+                                        onPressed: snapshot.data == true ? widget.presenter?.auth : null,
+                                        child: Text('Entrar'.toUpperCase())
                                     );
                                   }
                               ),
-                            ),
 
-                            StreamBuilder<bool>(
-                                stream: widget.presenter?.isFormValidErrorStream,
-                                builder: (context, snapshot) {
-                                  return ElevatedButton(
-                                      onPressed: snapshot.data == true ? widget.presenter?.auth : null,
-                                      child: Text('Entrar'.toUpperCase())
-                                  );
-                                }
-                            ),
-
-                            TextButton.icon(
-                                onPressed: () {},
-                                icon: Icon(Icons.person),
-                                label: Text('Criar Conta')
-                            )
-                          ],
-                        )
+                              TextButton.icon(
+                                  onPressed: () {},
+                                  icon: Icon(Icons.person),
+                                  label: Text('Criar Conta')
+                              )
+                            ],
+                          )
+                      ),
                     ),
                   )
                 ],
