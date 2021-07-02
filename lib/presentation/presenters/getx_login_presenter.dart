ebee0910f26d46703425a7925b7b33411226ea4c
paulocalazans <paulo.pc355@gmail.com>
2021-07-01 21:02:31 -0300

    remove hide loading in success case

--- lib/presentation/presenters/getx_login_presenter.dart
+++ lib/presentation/presenters/getx_login_presenter.dart
@@ -47,14 +47,14 @@ class GetxLoginPresenter extends GetxController implements LoginPresenter {
   }
 
   Future<void> auth() async {
-    _isLoading.value = true;
     try {
+      _isLoading.value = true;
       var accountEntity = await authentication.auth(AuthenticationParams(email: _email!, secret: _password!));
       await saveCurrentAccount.save(accountEntity);
     } on DomainError catch (error) {
       _mainError.value = error.description;
+      _isLoading.value = false;
     }
-    _isLoading.value = false;
   }
 
   void dispose() {}
