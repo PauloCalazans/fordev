a1b84c72a49054b568c99a6ca1edef84fa8d5abc
paulocalazans <paulo.pc355@gmail.com>
2021-07-17 14:51:11 -0300

    add delay in GetxSplashPresenter transition

--- lib/presentation/presenters/getx_splash_presenter.dart
+++ lib/presentation/presenters/getx_splash_presenter.dart
@@ -10,7 +10,8 @@ class GetxSplashPresenter implements SplashPresenter {
   GetxSplashPresenter({required this.loadCurrentAccount});
 
   @override
-  Future<void>? checkAccount() async {
+  Future<void>? checkAccount({int durationInSeconds = 2}) async {
+    await Future.delayed(Duration(seconds: durationInSeconds));
     try {
       var account = await loadCurrentAccount.load();
       _navigateTo.value = account == null ? '/login' : '/surveys';
