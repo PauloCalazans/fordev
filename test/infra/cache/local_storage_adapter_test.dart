ea9b43a0bc1bcf6e47099744a180ed8a9724538b
paulocalazans <paulo.pc355@gmail.com>
2021-07-17 11:34:39 -0300

    ensure LocalStorageAdpater returns correct value

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -39,10 +39,24 @@ void main() {
   });
 
   group('fetchSecure', () {
+    void mockFetchSecure() {
+      when(() => secureStorage.read(key: any(named: 'key'))).thenAnswer((_) async => value);
+    }
+
+    setUp(() {
+      mockFetchSecure();
+    });
+
     test('Should call fetch secure with correct value', () async {
       await sut.fetchSecure(key);
 
       verify(() => secureStorage.read(key: key));
+    });
+
+    test('Should return correct value on sucess', () async {
+      final fetchedValue = await sut.fetchSecure(key);
+
+      expect(fetchedValue, value);
     });
   });
 
