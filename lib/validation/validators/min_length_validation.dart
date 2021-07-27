e6e58715d8047102b271048cae177a30b1461519
paulocalazans <paulo.pc355@gmail.com>
2021-07-27 20:05:38 -0300

    move MinLengthValidation to its own file

---
+++ lib/validation/validators/min_length_validation.dart
@@ -0,0 +1,15 @@
+import '../../presentation/presenters/dependencies/dependencies.dart';
+
+import '../dependencies/dependencies.dart';
+
+class MinLengthValidation implements FieldValidation {
+  final String field;
+  final int size;
+
+  MinLengthValidation({required this.field, required this.size});
+
+  @override
+  ValidationError? validate(String? value) {
+    return value != null && value.length >= size ? null : ValidationError.invalidField;
+  }
+}
--- test/validation/validators/min_length_validation_test.dart
+++ test/validation/validators/min_length_validation_test.dart
@@ -1,21 +1,8 @@
 import 'package:faker/faker.dart';
 import 'package:test/test.dart';
 
-import 'package:fordev/validation/dependencies/dependencies.dart';
 import 'package:fordev/presentation/presenters/dependencies/dependencies.dart';
 import 'package:fordev/validation/validators/validators.dart';
-
-class MinLengthValidation implements FieldValidation {
-  final String field;
-  final int size;
-
-  MinLengthValidation({required this.field, required this.size});
-
-  @override
-  ValidationError? validate(String? value) {
-    return value != null && value.length >= size ? null : ValidationError.invalidField;
-  }
-}
 
 void main() {
   late MinLengthValidation sut;
