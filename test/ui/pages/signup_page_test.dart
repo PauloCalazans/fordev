7be2b7318bfd7c8eb6a590763f30480f1d2ba54c
paulocalazans <paulo.pc355@gmail.com>
2021-07-31 12:57:56 -0300

    remove unused testes

--- test/ui/pages/signup_page_test.dart
+++ test/ui/pages/signup_page_test.dart
@@ -74,43 +74,6 @@ void main() {
     closeStreamns();
   });
 
-  
-  testWidgets('Should load with correct initial state', (WidgetTester tester) async {
-    await loadPage(tester);
-
-    final nameTextChildren = find.descendant(of: find.bySemanticsLabel('Nome'), matching: find.byType(Text));
-    expect(
-      nameTextChildren,
-      findsOneWidget,
-      reason: 'when a TextFormField has only one text child, means it has no erros, since one of the childs is always the label text'
-    );
-
-    final emailTextChildren = find.descendant(of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
-    expect(
-      emailTextChildren,
-      findsOneWidget,
-      reason: 'when a TextFormField has only one text child, means it has no erros, since one of the childs is always the label text'
-    );
-
-    final passwordTextChildren = find.descendant(of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
-    expect(
-        passwordTextChildren,
-        findsOneWidget,
-        reason: 'when a TextFormField has only one text child, means it has no erros, since one of the childs is always the label text'
-    );
-
-    final passwordConfimationTextChildren = find.descendant(of: find.bySemanticsLabel('Confirmar Senha'), matching: find.byType(Text));
-    expect(
-        passwordConfimationTextChildren,
-        findsOneWidget,
-        reason: 'when a TextFormField has only one text child, means it has no erros, since one of the childs is always the label text'
-    );
-    
-    final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
-    expect(button.onPressed, null);
-    expect(find.byType(CircularProgressIndicator), findsNothing);
-  });
-
   testWidgets('Should call validate with correct values', (WidgetTester tester) async {
     await loadPage(tester);
 
