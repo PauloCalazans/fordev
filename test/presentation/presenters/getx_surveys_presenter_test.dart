35f6647cb891196a734dbbd184ab4d57af0c8df4
paulocalazans <paulo.pc355@gmail.com>
2021-08-02 19:45:13 -0300

    ensure GetxSurveysPresenter calls LoadSuruveys on loadData

---
+++ test/presentation/presenters/getx_surveys_presenter_test.dart
@@ -0,0 +1,26 @@
+import 'package:fordev/domain/usecases/load_surveys.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+class GetxSurveysPresenter {
+  final LoadSurveys loadSurveys;
+
+  GetxSurveysPresenter({required this.loadSurveys});
+
+  Future<void>? loadData() async {
+    await loadSurveys.load();
+  }
+}
+
+class LoadSurveysSpy extends Mock implements LoadSurveys {}
+
+void main() {
+  test('Should call LoadSurveys on loadData', () async {
+    final loadSurveys = LoadSurveysSpy();
+    final sut = GetxSurveysPresenter(loadSurveys: loadSurveys);
+
+    await sut.loadData();
+
+    verify(() => loadSurveys.load()).called(1);
+  });
+}
