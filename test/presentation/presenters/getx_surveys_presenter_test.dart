683a756b57dd5ce458d3ca52463ed2a5d280aee3
paulocalazans <paulo.pc355@gmail.com>
2021-08-02 20:59:09 -0300

    move GetxSurveysPresenter to its own file

--- test/presentation/presenters/getx_surveys_presenter_test.dart
+++ test/presentation/presenters/getx_surveys_presenter_test.dart
@@ -1,44 +1,15 @@
 import 'package:test/test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:faker/faker.dart';
-import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 
 import 'package:fordev/domain/entities/entities.dart';
 import 'package:fordev/domain/helpers/helpers.dart';
 import 'package:fordev/domain/usecases/usecases.dart';
+
 import 'package:fordev/ui/helpers/errors/errors.dart';
 import 'package:fordev/ui/pages/pages.dart';
 
-class GetxSurveysPresenter {
-  final LoadSurveys loadSurveys;
-
-  final _isLoading = true.obs;
-  final _surveys = Rxn<List<SurveyViewModel>>();
-
-  Stream<bool?> get isLoadingStream => _isLoading.stream;
-  Stream<List<SurveyViewModel>?> get surveysStream => _surveys.stream;
-
-  GetxSurveysPresenter({required this.loadSurveys});
-
-  Future<void>? loadData() async {
-    try {
-      _isLoading.value = true;
-      final surveys = await loadSurveys.load();
-      _surveys.value = surveys!.map((survey) =>
-          SurveyViewModel(
-              id: survey.id,
-              question: survey.question,
-              date: DateFormat('dd MMM yyyy').format(survey.dateTime),
-              didAnswer: survey.didAnswer
-          )).toList();
-    } on DomainError {
-      _surveys.addError(UIError.unexpected.description);
-    } finally {
-      _isLoading.value = false;
-    }
-  }
-}
+import 'package:fordev/presentation/presenters/presenters.dart';
 
 class LoadSurveysSpy extends Mock implements LoadSurveys {}
 
