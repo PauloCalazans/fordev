da3acfe6e48590ef18bce34feadc08476d804c55
paulocalazans <paulo.pc355@gmail.com>
2021-08-05 20:21:47 -0300

    ensure Decorator calls FetchSecureCacheStorage with correct key

---
+++ test/main/decorator/authorize_http_client_decorator/authorize_http_client_decorator_test.dart
@@ -0,0 +1,32 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+import 'package:fordev/data/cache/cache.dart';
+
+class AuthorizeHttpClientDecorator {
+  final FetchSecureCacheStorage fetchSecureCacheStorage;
+
+  AuthorizeHttpClientDecorator({required this.fetchSecureCacheStorage});
+
+  Future<void> request() async {
+    await fetchSecureCacheStorage.fetchSecure('token');
+  }
+}
+
+class FetchSecureCacheStorageSpy extends Mock implements FetchSecureCacheStorage {}
+
+void main() {
+
+  setUp(() {});
+
+  test('Should call FetchSecureCacheStorage with correct key', () async {
+    final fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
+    final sut = AuthorizeHttpClientDecorator(
+        fetchSecureCacheStorage: fetchSecureCacheStorage
+    );
+
+    await sut.request();
+
+    verify(() => fetchSecureCacheStorage.fetchSecure('token')).called(1);
+  });
+}
