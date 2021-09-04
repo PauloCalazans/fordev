9e44689a8a5461101373956a0640e387b0b65f63
paulocalazans <paulo.pc355@gmail.com>
2021-09-03 22:37:00 -0300

    ensure RemoteSaveSurveyResult throws DomainError if HttpClient error

--- lib/data/usecases/save_survey_result/remote_save_survey_result.dart
+++ lib/data/usecases/save_survey_result/remote_save_survey_result.dart
@@ -1,3 +1,5 @@
+import '../../../domain/helpers/helpers.dart';
+
 import '../../http/http.dart';
 
 class RemoteSaveSurveyResult{
@@ -7,6 +9,12 @@ class RemoteSaveSurveyResult{
   RemoteSaveSurveyResult({required this.url, required this.httpClient});
 
   Future<void>? save({required String answer}) async {
-    await httpClient.request(url: url, method: 'put', body: {'answer': answer});
+    try {
+      final json = await httpClient.request(url: url, method: 'put', body: {'answer': answer});
+      //return RemoteSurveyResultModel.fromJson(json).toEntity();
+    } on HttpError catch(error) {
+      throw error == HttpError.forbidden ? DomainError.accessDenied : DomainError.unexpected;
+    }
+
   }
 }
--- test/data/usecases/save_survey_result/remote_save_survey_result_test.dart
+++ test/data/usecases/save_survey_result/remote_save_survey_result_test.dart
@@ -4,6 +4,7 @@ import 'package:mocktail/mocktail.dart';
 
 import 'package:fordev/data/http/http.dart';
 import 'package:fordev/data/usecases/usecases.dart';
+import 'package:fordev/domain/helpers/helpers.dart';
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
@@ -12,6 +13,18 @@ void main() {
   late String url;
   late HttpClientSpy httpClient;
   late String answer;
+
+  When mockRequest() => when(() =>
+      httpClient.request(
+        url: any(named: 'url'),
+        method: any(named: 'method'),
+        body: any(named: 'body')
+      )
+  );
+
+  void mockHttpError(HttpError error) {
+    mockRequest().thenThrow(error);
+  }
 
   setUp(() {
     url = faker.internet.httpUrl();
@@ -24,5 +37,29 @@ void main() {
     await sut.save(answer: answer);
 
     verify(() => httpClient.request(url: url, method: 'put', body: {'answer': answer}));
+  });
+
+  test('Should throw UnexpectedError if HttClient returns 404', () async {
+    mockHttpError(HttpError.notFound);
+
+    final future = sut.save(answer: answer);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpectedError if HttClient returns 500', () async {
+    mockHttpError(HttpError.serverError);
+
+    final future = sut.save(answer: answer);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpectedError if HttClient returns 403', () async {
+    mockHttpError(HttpError.forbidden);
+
+    final future = sut.save(answer: answer);
+
+    expect(future, throwsA(DomainError.accessDenied));
   });
 }
