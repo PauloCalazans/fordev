efcb69b346ca4907e85af9cff670e67de2420275
paulocalazans <paulo.pc355@gmail.com>
2021-09-04 10:39:44 -0300

    ensure SurveysPage reload data on page appear

--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 
 import '../../components/components.dart';
@@ -9,35 +10,54 @@ import '../../mixins/mixins.dart';
 import 'components/components.dart';
 import 'surveys_presenter.dart';
 
-class SurveysPage extends StatelessWidget with LoadingManager, NavigationManager, SessionManager {
+class SurveysPage extends StatefulWidget {
   final SurveysPresenter presenter;
 
   SurveysPage(this.presenter);
 
   @override
+  _SurveysPageState createState() => _SurveysPageState();
+}
+
+class _SurveysPageState extends State<SurveysPage> with LoadingManager, NavigationManager, SessionManager, RouteAware {
+
+  @override
+  void dispose() {
+    Get.find<RouteObserver>().unsubscribe(this);
+    super.dispose();
+  }
+
+  @override
+  void didPopNext() {
+    widget.presenter.loadData();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    Get.find<RouteObserver>().subscribe(this, ModalRoute.of(context)!);
+
     return Scaffold(
       appBar: AppBar(title: Text(R.strings.surveys)),
       body: Builder(
         builder: (context) {
-          handleLoading(presenter.isLoadingStream, context);
-          presenter.loadData();
-          handleNavigation(presenter.navigateToStream);
-          handleSessionExpired(presenter.isSessionExpiredStream);
+          handleLoading(widget.presenter.isLoadingStream, context);
+          handleNavigation(widget.presenter.navigateToStream);
+          handleSessionExpired(widget.presenter.isSessionExpiredStream);
+          widget.presenter.loadData();
 
           return StreamBuilder<List<SurveyViewModel>?>(
-            stream: presenter.surveysStream,
+            stream: widget.presenter.surveysStream,
             builder: (context, snapshot) {
               if(snapshot.hasError) {
                 return ReloadScreen(
                     error: '${snapshot.error}',
-                    reload: presenter.loadData
+                    reload: widget.presenter.loadData
                 );
               }
 
               if(snapshot.hasData) {
                 return Provider(
-                  create: (_) => presenter,
+                  create: (_) => widget.presenter,
                   child: Padding(
                     padding: const EdgeInsets.symmetric(vertical: 20),
                     child: CarouselSlider(
